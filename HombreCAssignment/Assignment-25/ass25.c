899b1c51c2776959f7c08bb369ccaed62b49fc5f
Tony Tran <tonytranme0@hotmail.com>
2025-07-12 14:43:33 +1000

    ass25.c code

---
+++ HombreCAssignment/Assignment-25/ass25.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main (void)
+{
+	int filedescriptor;
+
+	filedescriptor = open("testfile.txt", O_WRONLY | O_CREAT, S_IRWXU);
+
+	if (filedescriptor < 0)
+	{
+		printf("The open operation failed...");
+		return -1;
+	}
+	else 
+	{
+		printf("The open operation succeeded!\n");
+		
+	}
+
+	int writertn;
+
+	writertn = write(filedescriptor, "Writing test data to the file", 30);
+
+	if (writertn != 30)
+	{
+		printf("The write operation failed...");
+		return -1;
+	}
+	else
+	{
+		printf("The write operation succeeded!");
+	}
+
+	return 0;
+
+}
