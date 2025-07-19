0e8fc3166485ede021828ed5919811d4046c4e5a
Tony Tran <tonytranme0@hotmail.com>
2025-07-19 20:22:33 +1000

    Up to Aviz System Call

---
+++ C Aviv C 485h/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void world(void);
+void hello(void);
+
+
+void hello()
+{
+    printf("Hello ");
+
+}
+
+int main(int argc, char *argv[])
+{
+    hello();
+    world();
+    
+}
